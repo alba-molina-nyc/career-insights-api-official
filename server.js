33a2b0a70a8f88b9110f47de8bf259b12188f6e5
alba-molina-nyc <avmanyc@gmail.com>
2021-10-30 22:29:42 -0400

    pk etc. installed

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ const contactsController = require('./controllers/contacts');
 // const roadmapsController = require('./controllers/roadmaps');
 const admin = require('firebase-admin');
 
-
 // Initialize Express App
 const app = express();
 
 // Configure Settings
 require('dotenv').config();
-const { DATABASE_URL, PORT=3001 } = process.env;
+const { 
+  PORT=3001,
+  CLIENT_ID,
+  PRIVATE_KEY,  
+  DATABASE_URL, 
+  PRIVATE_KEY_ID, 
+        } = process.env;
 // Configure connection to MongoDB
 mongoose.connect(DATABASE_URL);
 const db = mongoose.connection;
@@ -27,24 +32,48 @@ app.use(cors()); // attaches a Access-Control-Allow-Origin header to the respons
 app.use(express.json()); // creates req.body
 app.use(morgan('dev'));
 
-// // Authorization Middleware
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-//   });
+
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": "career-insights-official",
+      "private_key_id": PRIVATE_KEY_ID,
+      "private_key": PRIVATE_KEY.replace(/\\n/g, '\n'),
+      "client_email": "firebase-adminsdk-alxt3@career-insights-official.iam.gserviceaccount.com",
+      "client_id": CLIENT_ID,
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-alxt3%40career-insights-official.iam.gserviceaccount.com"
+  })
+});
+
   
-//   app.use(function(req, res, next) {
-//       const token = req.get('Authorization')
-//       console.log(token);
-//       next();
-//   });
-  
+  app.use(async function(req, res, next) {
+      const token = req.get('Authorization')
+
+      if(token) {
+        const authUser = await admin.auth().verifyIdToken(token.replace('Bearer',''))
+        req.user = authUser;
+
+      }
+
+      next();
+  });
+
+  function isAuthenticated(req, res, next) {
+    if(req.user) return next();
+    else res.status(401).json({message: 'unauthorized'})
+
+  }
   
   // Mount Routes
   app.get('/api', (req, res) => {
       res.json({message: 'Welcome to the Career Insights API'})
   });
   
-  app.use('/api/contacts', contactsController);
+  app.use('/api/contacts', isAuthenticated, contactsController);
 //   app.use('/api/applications', applicationsController);
 //   app.use('/api/roadmaps', roadmapsController);
   
